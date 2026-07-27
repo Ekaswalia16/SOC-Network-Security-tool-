d813c5ecdde0dabd45187446ac4e4f3c960b2c83
Ekas Walia <ekaswalia4@gmail.com>
2026-07-27 12:52:23 +0530

    changessss

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = dirname(__filename)
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),  ],
+    tailwindcs  ],
 
   // For development (vite)
   server: {
